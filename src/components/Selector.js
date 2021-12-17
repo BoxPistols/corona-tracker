4807f264a226ca3318074555671838446e8297b8
aito <BoxPistols@users.noreply.github.com>
2021-12-17 22:38:52 +0900

    Add Api Base / Selecct Form Move Test

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,7 +1,19 @@
+import countriesApi from "../api/countries.json"
+
 export const Selector = () => {
+    // console.log(countriesApi);
     return (
-        <div className="Selector">
+        <div className="">
             <h1>Selector</h1>
+            <select>
+                {
+                    countriesApi.map((c) =>
+                        <option>
+                            {c.Country}
+                        </option>
+                    )
+                }
+            </select>
         </div>
-    );
+    )
 };
