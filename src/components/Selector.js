f4d5bd094df96cf052755483835e2926d688701a
aito <BoxPistols@users.noreply.github.com>
2021-12-25 06:04:06 +0900

    Add Slug

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,20 +1,24 @@
+import { useState } from "react";
 import countriesApi from "../api/countries.json"
 
 export const Selector = () => {
     // console.log(countriesApi);
+    const [country, setCountry] = useState(countriesApi[0].Country)
+
     return (
         <div className="">
             <h1>Selector</h1>
-            <select>
+            <select onChange={(e)=> setCountry(e.target.value)}>
                 {
                     countriesApi.map((country, index) =>
                     // @see https://ja.reactjs.org/docs/lists-and-keys.html#keys
-                        <option key={country.Country}>
+                        <option key={index} value={country.Slug}>
                             {index}:{country.Country}
                         </option>
                     )
                 }
             </select>
+            <p>{country}</p>
         </div>
     )
 };
