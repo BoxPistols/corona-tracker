0fa78828a27e19ca44a69ef62ab89b4ac1caa003
aito <BoxPistols@users.noreply.github.com>
2021-12-25 16:58:58 +0900

    Get API Change-Country by Select-Form

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -6,27 +6,27 @@ export const Selector = () => {
     const [country, setCountry] = useState(countriesApi[0].Country)
 
     const getCountryData = () => {
-        fetch("https://api.covid19api.com/country/japan")
-        .then(res=> res.json())
-        .then(data => console.log(data))
+        fetch(`https://api.covid19api.com/country/${country}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
     }
 
     return (
         <div className="">
             <h1>Selector</h1>
-            <select onChange={(e)=> setCountry(e.target.value)}>
+            <select onChange={(e) => setCountry(e.target.value)}>
+              <option>Select Country</option>
                 {
                     countriesApi.map((country, index) =>
-                    // @see https://ja.reactjs.org/docs/lists-and-keys.html#keys
-                        <option key={index} value={country.Slug}>
+                    <option key={index} value={country.Slug}>
                             {index}:{country.Country}
                         </option>
                     )
                 }
             </select>
-            <p>{country}</p>
             <button onClick={getCountryData}>Get Data</button>
         </div>
     )
-
 };
+
+// @see for key-on-map https://ja.reactjs.org/docs/lists-and-keys.html#keys
