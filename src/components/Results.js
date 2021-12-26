1a373dd6d8849f1d98e2a5cbbf36a9a2b08b0da8
aito <BoxPistols@users.noreply.github.com>
2021-12-26 20:45:23 +0900

    Fix UI

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,19 +5,19 @@ export const Results = (props) => {
       <h1>Results</h1>
       <dl>
         <dt>日付：</dt>
-        <dd>{props.countryData.dateGet}</dd>
+        <dd>{props.countryData.dateGet.slice(0, 10)}</dd>
         <dt>新規感染者：</dt>
-        <dd>{props.countryData.newConfirmed}</dd>
+        <dd>{props.countryData.newConfirmed.toLocaleString()}</dd>
         <dt>感染者総数：</dt>
-        <dd>{props.countryData.totalConfirmed}</dd>
+        <dd>{props.countryData.totalConfirmed.toLocaleString()}</dd>
         <dt>新規回復者：</dt>
-        <dd>{props.countryData.newRecoverd}</dd>
+        <dd>{props.countryData.newRecoverd.toLocaleString()}</dd>
         <dt>回復者総数：</dt>
-        <dd>{props.countryData.totalRecoverd}</dd>
+        <dd>{props.countryData.totalRecoverd.toLocaleString()}</dd>
         <dt>新規死者：</dt>
-        <dd>{props.countryData.newDeaths}</dd>
+        <dd>{props.countryData.newDeaths.toLocaleString()}</dd>
         <dt>死者総数：</dt>
-        <dd>{props.countryData.totalDeaths}</dd>
+        <dd>{props.countryData.totalDeaths.toLocaleString()}</dd>
       </dl>
       <p>*一部正確でない数値もあります</p>
     </div >
